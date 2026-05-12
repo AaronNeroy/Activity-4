2571ec2073049a6e98460cac54ebbad23a27c530
AaronNeroy <aaron_neroy@yahoo.com.au>
2026-05-12 18:26:15 +1000

    Update entrypoint.sh

--- .github/scripts/entrypoint.sh
+++ .github/scripts/entrypoint.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+set -e
 
+SCRIPTS_DIR="/app/.github/scripts"
+
+echo "Running todo.py..."
+python3 "$SCRIPTS_DIR/todo.py" | tee /tmp/todo_output.txt
+
+echo "Running todo-test.py..."
+python3 "$SCRIPTS_DIR/todo-test.py" | tee /tmp/test_output.txt
+
+echo "Updating index.html..."
+bash "$SCRIPTS_DIR/update_index.sh" /tmp/todo_output.txt /tmp/test_output.txt
+
+echo "Entrypoint complete."
